4947dc7d41361953beac6f67309ca281824e2adf
Jayx20 <Jayx20@users.noreply.github.com>
2020-07-27 20:05:42 -0500

    Test map loading and drawing Not the prettiest or most flexible code but it is a good prototype

--- Game1.cs
+++ Game1.cs
@@ -2,16 +2,25 @@
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 
+using System.Collections.Generic;
+
+using TiledSharp;
+
 namespace Prototype_Golem
 {
     public class Game1 : Game
     {
         private GraphicsDeviceManager _graphics;
-        private SpriteBatch _spriteBatch;
+        private SpriteBatch spriteBatch;
+
+        TmxMap testmap1;
+        Dictionary<string, Texture2D> tilemapTextures = new Dictionary<string, Texture2D>();
 
         public Game1()
         {
             _graphics = new GraphicsDeviceManager(this);
+            _graphics.PreferredBackBufferWidth = 1280;
+            _graphics.PreferredBackBufferHeight = 720;
             Content.RootDirectory = "Content";
             IsMouseVisible = true;
         }
@@ -20,12 +29,18 @@ namespace Prototype_Golem
         {
             // TODO: Add your initialization logic here
 
+            testmap1 = new TmxMap("Maps/test1.tmx");
+
+            Texture2D prototexture = Content.Load<Texture2D>("Images/prototype1"); //this variable will dissapear so the stupid name is fine the actual data is saved in the dictionary
+
+            tilemapTextures.Add("prototype1", prototexture);
+
             base.Initialize();
         }
 
         protected override void LoadContent()
         {
-            _spriteBatch = new SpriteBatch(GraphicsDevice);
+            spriteBatch = new SpriteBatch(GraphicsDevice);
 
             // TODO: use this.Content to load your game content here
         }
@@ -44,7 +59,43 @@ namespace Prototype_Golem
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
-            // TODO: Add your drawing code here
+            TmxTileset baseTileset = testmap1.Tilesets["prototype1"];
+            int columns = baseTileset.Columns ?? -1;
+            if (columns == -1) {
+                throw new System.ArgumentException("HOW");
+            } 
+
+            //i should probably make a class or something that combines the TmxTileset and the actual Texture2D or just find a way to load the texture from the tileset instead of from monogames content manager
+            //TODO: make this less stupid
+
+            Texture2D tilesetTexture;
+
+            if (!tilemapTextures.TryGetValue("prototype1", out tilesetTexture)) { //"prototype1" would be a variable after we find what tileset the map uses
+                //runs if loading the image failed
+                throw new System.ArgumentException("Loading tilemap texture failed.");
+            }
+
+            TmxLayer baseLayer = testmap1.Layers["base"];
+
+            spriteBatch.Begin();
+
+            var tiles = baseLayer.Tiles;
+            foreach(TmxLayerTile tile in tiles) {
+                if (tile.Gid == 0) continue; //air
+                
+                Rectangle destRect = new Rectangle(tile.X*testmap1.TileWidth, tile.Y*testmap1.TileHeight, testmap1.TileWidth, testmap1.TileHeight); //where on the screen the tile is going to be drawn, based on the position of the tile
+
+                int textureId = tile.Gid - baseTileset.FirstGid; 
+
+                //funny math that has to do with 2d arrays and stuff
+                    int textureX = textureId % columns;
+                    int textureY = textureId / columns;
+
+                Rectangle sourceRect = new Rectangle(textureX*testmap1.TileWidth, textureY*testmap1.TileHeight, testmap1.TileWidth, testmap1.TileHeight);
+                spriteBatch.Draw(tilesetTexture, destRect, sourceRect, Color.White);
+            }
+
+            spriteBatch.End();
 
             base.Draw(gameTime);
         }
